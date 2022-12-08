b9f20f2a8300bd1e64f94767a665147d56b68897
diegomadariaga <diegonicolasmadariaga@gmail.com>
2022-12-08 01:14:22 -0300

    user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
 
-@Module({})
+@Module({
+  providers: [UsersService],
+})
 export class UsersModule {}
